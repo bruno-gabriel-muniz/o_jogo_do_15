eb115be8b4d6f29553127a4c803b5f891419fc7b
Bruno <bruno_gabrial_muniz@outlook.pt>
2023-01-26 14:29:22 -0300

    Versão: 0.7| Recebe e verifica se o movimento do jogador é legal, assim como, chamará a função da realização do movimento.

--- teste.py
+++ teste.py
@@ -45,13 +45,58 @@ def mostra_matriz():
         v3=i[2]
         v4=i[3]
         print("|%2i|%2i|%2i|%2i|"%(v1,v2,v3,v4))
+    print("-"*13)
 #função que mostra os movimentos disponivei para o jogador
 def movimentos():
     #print das setas e as teclas para acionar o movimento
     print('''       /ŵ\\
  <--a       d-->
        \s/''')
-#criando a função da jogada do usuário
+#criando a função que recebe e verifica se o movimento é legal
+def recebe_movimento():
+    global matriz_real
+    #pedir a escolha do usuário
+    jogada=input("escolha a sua jogada: ")
+    #verificar se foi para cima
+    if jogada == "w":
+        #verificar se o movimento é legal
+        if matriz_real[0][0]!= 0 != matriz_real[0][1] and matriz_real[0][2]!= 0 != matriz_real[0][3]:
+            #se sim realizar o movimento
+            print("realizar o movimento")
+        #se não
+        else:
+            #devolver erro
+            print("não realizar o movimento")
+    #verificar se foi para baixo
+    elif jogada == "s":
+        #verificar se o movimento é legal
+        if matriz_real[3][0]!= 0 != matriz_real[3][1] and matriz_real[3][2]!= 0 != matriz_real[3][3]:
+            #se sim realizar o movimento
+            print("realizar o movimento")
+        #se não
+        else:
+            #devolver erro
+            print("não realizar o movimento")
+    #verificar se foi para a esquerda
+    elif jogada == "a": 
+        #verificar se o movimento é legal
+        if matriz_real[0][0]!= 0 != matriz_real[1][0] and matriz_real[2][0]!= 0 != matriz_real[3][0]:
+            #se sim realizar o movimento
+            "realizar o movimento"
+        #se não
+        else:
+            #devolver erro
+            print("não realizar o movimento")
+    #verificar se foi para a direita
+    elif jogada == "d":
+#verificar se o movimento é legal
+        if matriz_real[0][3]!= 0 != matriz_real[1][3] and matriz_real[2][3]!= 0 != matriz_real[3][3]:
+            print("realizar o movimento")
+        #se não
+        else:
+            #devolver erro
+            print("não realizar o movimento")
 matriz_real=cria_matriz_4x4()
 mostra_matriz()
 movimentos()
+recebe_movimento()
